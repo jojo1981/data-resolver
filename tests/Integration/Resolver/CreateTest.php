cc6140749459471984c5a0af6b10892d9b4c1dd4
Joost Nijhuis <jnijhuis81@gmail.com>
2019-10-23 14:23:27 +0200

    Add create resolver integration test.

---
+++ tests/Integration/Resolver/CreateTest.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of the jojo1981/data-resolver package
+ *
+ * Copyright (c) 2019 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+namespace tests\Jojo1981\DataResolver\Integration\Resolver;
+
+use Jojo1981\DataResolver\Builder\ResolverBuilder;
+use Jojo1981\DataResolver\Extractor\Exception\ExtractorException;
+use Jojo1981\DataResolver\Handler\Exception\HandlerException;
+use Jojo1981\DataResolver\Predicate\Exception\PredicateException;
+use PHPUnit\Framework\ExpectationFailedException;
+use SebastianBergmann\RecursionContext\InvalidArgumentException;
+use tests\Jojo1981\DataResolver\Integration\AbstractIntegrationTestCase;
+
+/**
+ * @package tests\Jojo1981\DataResolver\Integration\Resolver
+ */
+class CreateTest extends AbstractIntegrationTestCase
+{
+    /**
+     * @test
+     * @coversNothing
+     *
+     * @return void
+     * @throws InvalidArgumentException
+     * @throws ExtractorException
+     * @throws HandlerException
+     * @throws PredicateException
+     * @throws ExpectationFailedException
+     */
+    public function checkCreate(): void
+    {
+        $resolverBuilder = $this->getResolverBuilderFactory()->create();
+        $this->assertInstanceOf(ResolverBuilder::class, $resolverBuilder);
+        $resolver = $resolverBuilder->build();
+
+        $this->assertEquals(true, $resolver->resolve(true));
+        $this->assertEquals(false, $resolver->resolve(false));
+        $this->assertEquals(-1, $resolver->resolve(-1));
+        $this->assertEquals(1, $resolver->resolve(1));
+        $this->assertEquals(-1.2, $resolver->resolve(-1.2));
+        $this->assertEquals(1.2, $resolver->resolve(1.2));
+        $this->assertEquals(10, $resolver->resolve(10));
+        $this->assertEquals(new \stdClass(), $resolver->resolve(new \stdClass()));
+        $this->assertEquals('text', $resolver->resolve('text'));
+        $this->assertEquals('true', $resolver->resolve('true'));
+        $this->assertEquals('false', $resolver->resolve('false'));
+        $this->assertEquals('1', $resolver->resolve('1'));
+        $this->assertEquals([1, 2, 3], $resolver->resolve([1, 2, 3]));
+        $this->assertEquals(['zero', 'one', 'two'], $resolver->resolve(['zero', 'one', 'two']));
+        $this->assertEquals([1 => 'one', 2 => 'two'], $resolver->resolve([1 => 'one', 2 => 'two']));
+        $this->assertEquals(['one' => 1, 'two' => 2, 'three' => 3], $resolver->resolve(['one' => 1, 'two' => 2, 'three' => 3]));
+        $this->assertEquals(0, $resolver->resolve(0));
+        $this->assertEquals('', $resolver->resolve(''));
+        $this->assertEquals('0', $resolver->resolve('0'));
+        $this->assertEquals(null, $resolver->resolve(null));
+        $this->assertEquals([], $resolver->resolve([]));
+    }
+}
