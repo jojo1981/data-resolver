494e9850c914b1cf517898ca4a3978fee32515dc
Joost Nijhuis <jnijhuis81@gmail.com>
2020-04-24 14:05:26 +0200

    Enabled testing of final classes.

---
+++ tests/autoload.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of the jojo1981/data-resolver package
+ *
+ * Copyright (c) 2020 Joost Nijhuis <jnijhuis81@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed in the root of the source code
+ */
+require __DIR__ . '/../vendor/autoload.php';
+
+DG\BypassFinals::enable();
